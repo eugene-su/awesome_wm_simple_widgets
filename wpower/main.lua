eaff872fa125a6fa498b430a513235ee7848b2f1
Eugene Bakhtin <55482490+eugene-su@users.noreply.github.com>
2021-03-09 21:27:09 +0300

    Update main.lua

--- wpower/main.lua
+++ wpower/main.lua
@@ -2,7 +2,7 @@
 
 Power watchdog widget for awesome wm.
 Tested with Lua 5.3.5.
-Copyright (c) 2019 Evgeny.
+Copyright (c) 2019 Eugene Bakhtin <mail@eugene.su>.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
